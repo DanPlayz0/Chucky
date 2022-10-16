e4f198dabb60f0a33dc8ccc1b052cd5347c2f336
DanPlayz <damuffin36@gmail.com>
2022-10-15 23:55:01 -0700

    Removed useless things

--- config-private.example.js
+++ config-private.example.js
@@ -1,8 +1,6 @@
 module.exports = {
   token: "DISCORD_BOT_TOKEN",
 
-  mongo_uri: "mongodb://username:password@localhost:27017/chucky?authSource=admin&retryWrites=true&ssl=false",
-
   webhooks: {
     shard: { id: "", token: "" },
     error: { id: "", token: "" },
--- config.js
+++ config.js
@@ -4,10 +4,7 @@ module.exports = {
   token: private.token,
 
   // Bot Administators (Access to Admin Dash & System Commands)
-  admins: ['209796601357533184', '813875382972973088'],
-  
-  // Database Crap (MongoDB & Redis)
-  mongo_uri: private.mongo_uri,
+  admins: ['209796601357533184'],
   
   // Support server. (For the "dashboard.example/join")
   supportServerInvite: "https://discord.gg/KkS6yP8",
